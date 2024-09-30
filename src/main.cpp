1e821fd6db5785a683c134f6631db607c662f754
galileo-dev <fionn2200@gmail.com>
2024-09-30 14:41:52 +0100

    parse cli argument

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,13 @@
-#include "inverted_index.h"
+#include "settings.h"
+#include <iostream>
+#include <optional>
+#include <string>
+#include <vector>
 
-int main() { InvertedIndex searchEngine; }
+int main(int argc, const char *argv[]) {
+  CLISettings settings = parse_settings(argc, argv);
+
+  if (settings.help) {
+    usage(argv[0]);
+  }
+}
