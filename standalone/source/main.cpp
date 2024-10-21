b0b5f2c36ce51a091186b5d13cf2f4ae6a39ab02
galileo-dev <fionn2200@gmail.com>
2024-10-21 11:16:46 +0100

    Handle multiple files

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -2,6 +2,7 @@
 
 #include <cereal/archives/binary.hpp>
 #include <cxxopts.hpp>
+#include <filesystem>
 #include <iostream>
 #include <optional>
 #include <sstream>
@@ -53,16 +54,33 @@ int main(int argc, const char *argv[]) {
     return 0;
   }
 
-  // Main commands
   if (settings.count("add")) {
     std::string document_path = settings["add"].as<std::string>();
-    std::string base_filename = document_path.substr(document_path.find_last_of("/\\") + 1);
 
-    searchEngine.add_document_from_file(base_filename, document_path);
-    for (auto &key : searchEngine.keys()) {
+    if (std::filesystem::is_directory(document_path)) {
+      // handle directory
+      for (const auto &entry : std::filesystem::directory_iterator(document_path)) {
+        if (entry.is_regular_file() && entry.path().extension() == ".txt") {
+          std::string file_path = entry.path().string();
+          std::string base_filename = entry.path().filename().string();
+
+          searchEngine.add_document_from_file(base_filename, file_path);
+          spdlog::info("Added document: {}, Path: {}", base_filename, file_path);
+        }
+      }
+    } else if (std::filesystem::is_regular_file(document_path)) {
+      // handle regular file
+      std::string base_filename = std::filesystem::path(document_path).filename().string();
+      searchEngine.add_document_from_file(base_filename, document_path);
+      spdlog::info("Added document: {}, Path: {}", base_filename, document_path);
+    } else {
+      spdlog::error("Invalid path provided: {}", document_path);
+    }
+
+    // update autocomplete trie
+    for (const auto &key : searchEngine.keys()) {
       autoComplete.insertKey(key);
     }
-    spdlog::info("Added document: {}, Path: {}", base_filename, document_path);
   }
 
   if (settings.count("debug")) {
